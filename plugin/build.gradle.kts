25b591dfbf03f49cfadc0b95286ce80a6c922fe8
tox1cozZ <Agravaine25@yandex.ru>
2022-08-04 15:42:34 +0300

    Applied shadow plugin and collected dependencies into fat jar. Minimal Gradle version is 5.6.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -5,14 +6,12 @@ plugins {
     `java-gradle-plugin`
     `maven-publish`
     id("com.gradle.plugin-publish") version "1.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-val shade: Configuration by configurations.creating
-configurations.compileOnly.get().extendsFrom(shade)
-
 dependencies {
-    shade(kotlin("stdlib-jdk8")) // implementation
-    shade("org.ow2.asm", "asm-tree", "9.3")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.ow2.asm:asm-tree:9.3")
     api(project(":tools"))
 }
 
@@ -22,13 +21,42 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("")
+    relocate("org.objectweb.asm", "io.github.tox1cozz.cutter.lib.org.objectweb.asm")
+    relocate("kotlin", "io.github.tox1cozz.cutter.lib.kotlin")
+    dependencies {
+        exclude(dependency("org.jetbrains:annotations"))
+    }
+}
+
 tasks.named<Jar>("jar") {
-    from(shade.map { if (it.isDirectory) it else zipTree(it) }) {
-        exclude("META-INF/*.SF")
-        exclude("META-INF/*.DSA")
-        exclude("META-INF/*.RSA")
-        exclude("META-INF/INDEX.LIST")
-        exclude("**/module-info.class")
+    enabled = false
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            named<MavenPublication>("pluginMaven") {
+                pom {
+                    name.set("cutter-gradle-plugin")
+                    description.set("A Gradle plugin for splitting code into client and server builds")
+                    url.set("https://github.com/tox1cozZ/cutter-gradle-plugin")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("tox1cozZ")
+                            name.set("Dmytro Tukin")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
